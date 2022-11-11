959151798bab930fe53aa4ce121a4979bd78e1ca
mapavaladares <marcospaulofsa14@gmail.com>
2022-11-10 21:40:17 -0300

    repo sobre variavéveis let e algumas utilidades dela

---
+++ variaveis-const.js
@@ -0,0 +1,3 @@
+// tipo string const que n varia//
+const primeiroNome = "Mapa";
+console.log(primeiroNome);
---
+++ variaveis-number.js
@@ -0,0 +1,8 @@
+// tipo number let que pode variar //
+let idade = 26;
+console.log(idade);
+// obs variaveis começa com letra minuscula//
+let alturaEmMetros = 1.70;
+// camelCase(proximaPalavraComecaMaiuscula)//
+//string template//
+console.log(`Meu nome é Mapa e minha idade é ${idade} e minha altura é ${1.70}`)
